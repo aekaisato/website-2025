a1f6962048f33adc25d48540081e55129d5762c8
Aiden Sato <aiden@aekai.dev>
2024-12-30 23:31:15 -0800

    small improvements

--- src/lib/util/constants.ts
+++ src/lib/util/constants.ts
@@ -2,7 +2,7 @@ export const Constants = Object.freeze({
   PORTFOLIO_DEFAULT_PRIORITY: 0,
   DATESLUG_SEPARATOR: "_",
   SITE_NAME: "aekai",
-  SITE_DESCRIPTION: "ramblings and stuff, ig...",
+  SITE_DESCRIPTION: "Some writings and ramblings of one Aiden Sato",
   SITE_URL: "https://aekai.dev",
   TITLE_SEPARATOR: "|"
 })
--- src/routes/(include-navbar)/blog/+page.svelte
+++ src/routes/(include-navbar)/blog/+page.svelte
@@ -11,6 +11,7 @@
 
 <div class="content-container">
   <h1>Blog</h1>
+  <a href="/blog/rss.xml" target="_blank"><i>(rss feed)</i></a>
   <ul>
     {#each posts as p}
       <li><a href="/blog/{p.slug}">{p.date} | {p.metadata.name}</a></li>
